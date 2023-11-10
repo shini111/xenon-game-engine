f0387c44e130bd86f367a6062c80c61f8aee758d
shini111 <xaviultrapro@gmail.com>
2023-11-10 15:12:10 +0000

    Testing gitignore

--- Aplication.cpp
+++ Aplication.cpp
@@ -7,8 +7,7 @@
 
 
 
-//This is a test
-
+//Should be working now
 static SDL_Surface* OptimizedSurface(std::string filepath, SDL_Surface* windowSurface) {
 	SDL_Surface* optimizedSurface = nullptr;
 	SDL_Surface* surface = SDL_LoadBMP(filepath.c_str());
