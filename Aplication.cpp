cd5e8d17025eb79dba4b1a4796d6349534348903
xavier <xaviultrapro@gmail.com>
2023-11-10 15:17:50 +0000

    testing ignore again

--- Aplication.cpp
+++ Aplication.cpp
@@ -7,7 +7,7 @@
 
 
 
-//Should be working now
+//Plz work now
 static SDL_Surface* OptimizedSurface(std::string filepath, SDL_Surface* windowSurface) {
 	SDL_Surface* optimizedSurface = nullptr;
 	SDL_Surface* surface = SDL_LoadBMP(filepath.c_str());
